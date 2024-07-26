ee8a732564e97fbefd945cba3ceddc140bc07d07
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-26 21:55:09 +0330

    feat (model) : add the model transaction

---
+++ app/Models/Transaction.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'account_id',
+        'amount',
+        'status',
+    ];
+
+    public function account(): BelongsTo
+    {
+        return $this->belongsTo(Account::class);
+    }
+
+    public function fees(): HasMany
+    {
+        return $this->hasMany(Fee::class);
+    }
+
+}

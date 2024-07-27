5b2c7e7c8423635b6703648ac20da5244a980119
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-27 15:29:44 +0330

    feat (controller) : AccountController add the index

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AccountController extends Controller
+{
+    public function index()
+    {
+        $accounts = Auth::user()->accounts;
+        return response()->json($accounts, 200);
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

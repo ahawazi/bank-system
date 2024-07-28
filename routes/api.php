7cbee4c343ba9769c3bd535b1b479eaebd327886
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-28 09:44:04 +0330

    feat (route) : api add the routs for accounts

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AccountController;
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/accounts', [AccountController::class, 'index'])->name('accounts.index');
+    Route::post('/accounts', [AccountController::class, 'store'])->name('accounts.store');
+    Route::get('/accounts/{id}', [AccountController::class, 'show'])->name('accounts.show');
+    Route::put('/accounts/{id}', [AccountController::class, 'update'])->name('accounts.update');
+    Route::delete('/accounts/{id}', [AccountController::class, 'destroy'])->name('accounts.destroy');
+});
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();

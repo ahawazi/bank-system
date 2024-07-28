96eebd4577acb7e039281e27b792a1cf1809eb00
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-28 09:40:00 +0330

    feat (factory) : add AccountFactory

---
+++ database/factories/AccountFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Account>
+ */
+class AccountFactory extends Factory
+{
+
+    protected $model = Account::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'account_number' => $this->faker->unique()->bankAccountNumber,
+            'phone_number' => $this->faker->phoneNumber,
+        ];
+    }
+}

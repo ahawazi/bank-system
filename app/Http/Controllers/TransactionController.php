d17cca58fe90daba919694bd5bf32bccdb0988ce
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-29 15:55:19 +0330

    feat (controller) : TransactionController add the index

---
+++ app/Http/Controllers/TransactionController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Transaction;
+use Illuminate\Http\Request;
+
+class TransactionController extends Controller
+{
+    public function index()
+    {
+        $accounts = Transaction::all();
+        return response()->json($accounts, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}

09b8a39fd84035f6f2763f8a48a994af955c2c62
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-29 15:55:44 +0330

    feat (factory) : TransactionFactory add the factory

---
+++ database/factories/TransactionFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Account;
+use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Transaction>
+ */
+class TransactionFactory extends Factory
+{
+    protected $model = Transaction::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'account_id' => Account::factory(),
+            'amount' => $this->faker->randomFloat(2, 0, 10000),
+            'status' => $this->faker->randomElement(['successful', 'failed']),
+        ];
+    }
+}

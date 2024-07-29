893b750f0eaa0321320af1b71241e5eec97582f3
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-29 15:56:54 +0330

    feat (api) : add the Transaction route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\TransactionController;
 
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/accounts', [AccountController::class, 'index'])->name('accounts.index');
@@ -10,6 +11,14 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/accounts/{id}', [AccountController::class, 'show'])->name('accounts.show');
     Route::put('/accounts/{id}', [AccountController::class, 'update'])->name('accounts.update');
     Route::delete('/accounts/{id}', [AccountController::class, 'destroy'])->name('accounts.destroy');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+    Route::post('/transactions', [TransactionController::class, 'store'])->name('transactions.store');
+    Route::get('/transactions/{id}', [TransactionController::class, 'show'])->name('transactions.show');
+    Route::put('/transactions/{id}', [TransactionController::class, 'update'])->name('transactions.update');
+    Route::delete('/transactions/{id}', [TransactionController::class, 'destroy'])->name('transactions.destroy');
 });
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {

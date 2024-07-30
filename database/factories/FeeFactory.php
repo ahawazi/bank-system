9c3cf4d252fc1d0ebc3151d5c5ed50752e02d2a5
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-30 11:52:02 +0330

    feat (factory) : add FeeFactory

---
+++ database/factories/FeeFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Transaction;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Fee>
+ */
+class FeeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'transaction_id' => Transaction::factory(),
+            'fee_amount' => $this->faker->randomFloat(2, 1, 100),
+        ];
+    }
+}

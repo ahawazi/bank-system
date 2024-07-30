4b481256b02feceeaec5775b73fe88e8232de7dc
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-30 12:13:01 +0330

    feat (route) : fee add the api route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
+use App\Http\Controllers\FeeController;
 use App\Http\Controllers\TransactionController;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -19,6 +20,14 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/transactions/{id}', [TransactionController::class, 'show'])->name('transactions.show');
     Route::put('/transactions/{id}', [TransactionController::class, 'update'])->name('transactions.update');
     Route::delete('/transactions/{id}', [TransactionController::class, 'destroy'])->name('transactions.destroy');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/fees', [FeeController::class, 'index'])->name('fees.index');
+    Route::post('/fees', [FeeController::class, 'store'])->name('fees.store');
+    Route::get('/fees/{id}', [FeeController::class, 'show'])->name('fees.show');
+    Route::put('/fees/{id}', [FeeController::class, 'update'])->name('fees.update');
+    Route::delete('/fees/{id}', [FeeController::class, 'destroy'])->name('fees.destroy');
 });
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {

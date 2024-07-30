fba18c951b70309d2c3a0b09d3beb491d0a96bc4
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-30 12:14:18 +0330

    feat (request) : add the StoreFeeRequest

---
+++ app/Http/Requests/StoreFeeRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreFeeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'transaction_id' => 'required|exists:transactions,id',
+            'fee_amount' => 'required|numeric|min:0',
+        ];
+    }
+}

bbb00a6cce7cdf1d76240aa553a90eb069c6c0f0
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-07-31 17:05:05 +0330

    fix (migration) : fix the fee_amount in the fees

--- database/migrations/2024_07_26_160912_create_fees_table.php
+++ database/migrations/2024_07_26_160912_create_fees_table.php
@@ -6,22 +6,16 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('fees', function (Blueprint $table) {
             $table->id();
             $table->foreignId('transaction_id')->constrained()->onDelete('cascade');
-            $table->decimal('fee_amount', 8, 2);
+            $table->decimal('fee_amount', 3, 0);
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('fees');

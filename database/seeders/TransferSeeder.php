550dff50ab7a2a2c4c4555986e8faf587d8a93d9
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-01 11:54:48 +0330

    feat (seeder ) : add the TransferSeeder

---
+++ database/seeders/TransferSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Transfer;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TransferSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Transfer::factory()->count(20)->create();
+    }
+}

9c00225c2a524831433fee8d91544089d403e505
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-01 11:55:58 +0330

    fix (migration) : add the inventory

--- database/migrations/2024_07_26_160841_create_accounts_table.php
+++ database/migrations/2024_07_26_160841_create_accounts_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('account_number')->unique();
+            $table->bigInteger('inventory')->default(0);
             $table->timestamps();
         });
     }

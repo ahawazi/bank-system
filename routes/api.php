77f3506509dcd9ba4428f2f3b01e94ef4772099e
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-02 01:01:00 +0330

    ref : transfer

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\FeeController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\TransferController;
 use App\Http\Middleware\RateLimitTransactions;
 
 Route::middleware('auth:sanctum')->group(function () {
@@ -15,10 +16,11 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::delete('/accounts/{id}', [AccountController::class, 'destroy']);
 });
 
+Route::post('/transfer', [TransferController::class, 'transfer'])->middleware(RateLimitTransactions::class);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/top-users-transactions', [TransactionController::class, 'topUsersTransactions']);
     Route::get('/balance', [TransactionController::class, 'balance']);
-    Route::post('/transfer', [TransactionController::class, 'transfer'])->middleware(RateLimitTransactions::class);
     Route::get('/transactions', [TransactionController::class, 'index']);
     Route::post('/transactions', [TransactionController::class, 'store'])->middleware(RateLimitTransactions::class);
     Route::get('/transactions/{id}', [TransactionController::class, 'show']);
@@ -27,9 +29,6 @@ Route::middleware('auth:sanctum')->group(function () {
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('/fees', [FeeController::class, 'index'])->name('fees.index');
     Route::post('/fees', [FeeController::class, 'store'])->name('fees.store');
-    Route::get('/fees/{id}', [FeeController::class, 'show'])->name('fees.show');
-    Route::put('/fees/{id}', [FeeController::class, 'update'])->name('fees.update');
-    Route::delete('/fees/{id}', [FeeController::class, 'destroy'])->name('fees.destroy');
 });
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {

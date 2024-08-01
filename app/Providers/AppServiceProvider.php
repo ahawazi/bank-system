844d7a19be5555083da4c7cc460567d4b8d85232
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-01 17:54:17 +0330

    register the event and listener

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,17 @@
 
 namespace App\Providers;
 
+use App\Events\TransactionOrTransferProcessed;
+use App\Events\TransactionProcessed;
+use App\Events\TransferCompleted;
+use App\Listeners\DeductTransactionFee;
+use App\Listeners\FeeDeducted;
 use App\Models\Account;
 use App\Models\Fee;
 use App\Policies\AccountPolicy;
 use App\Policies\FeePolicy;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 
@@ -25,8 +31,19 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Event::listen(
+            TransactionProcessed::class,
+            FeeDeducted::class,
+        );
+
+        Event::listen(
+            TransferCompleted::class,
+            FeeDeducted::class,
+        );
+
         Gate::policy(Fee::class, FeePolicy::class);
         Gate::policy(Account::class, AccountPolicy::class);
+
         ResetPassword::createUrlUsing(function (object $notifiable, string $token) {
             return config('app.frontend_url')."/password-reset/$token?email={$notifiable->getEmailForPasswordReset()}";
         });

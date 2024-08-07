72f8041fa0dcb1e513c8c05601a7882b3ca08219
AmirhosseinAvazi <141499905+AmirhossinAwazi@users.noreply.github.com>
2024-08-07 10:49:45 +0330

    feat : add the new commant AddUser

---
+++ app/Console/Commands/AddUser.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AddUser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:add-user';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
